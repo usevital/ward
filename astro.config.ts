722186309e75bc265be024bc3f1a4375d138ac73
Nitrrine <nitrrine@team.lodine.xyz>
2024-10-26 14:38:56 +0500

    Update site URL in astro config

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import compress from "astro-compress"
 import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  site: "https://ward-pwa.ward-5br.pages.dev/",
+  site: "https://usevital.github.io/ward/",
   base: "ward",
   integrations: [sitemap(), compress()],
   vite: {
